1d9998e869a1e6460b5a2fad6268604ef118c567
Muhammad Haider Shahab <mhaidershahab@technologyrivers.com>
2023-12-18 15:26:09 +0500

    Adding base URL

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -8,7 +8,7 @@ const LoginForm = ({ onLogin }) => {
   const navigate = useNavigate()
   const handleLogin = async () => {
     // Perform validation and send login request to the server
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('https://quicksight-demo-app-p89b.vercel.app/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
--- client/src/QuicksightDashboard.js
+++ client/src/QuicksightDashboard.js
@@ -6,12 +6,11 @@ const QuicksightDashboard = () => {
     <div>
       <h2>SMGB Users and Scanning Analytics</h2>
       {
-   
    <iframe
    width="1680"
    height="1000"
    src="https://us-east-1.quicksight.aws.amazon.com/sn/embed/share/accounts/330775944914/dashboards/78ef0f77-beef-456d-9731-b4ef0de26647?directory_alias=mhaider">
-</iframe>
+  </iframe>
     }
     </div>
   );
